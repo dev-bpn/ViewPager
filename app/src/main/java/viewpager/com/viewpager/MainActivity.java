e9738b870ece984147ccc907a6760a5ece202dfd
dev-bpn <jazzbpn@gmail.com>
2015-10-18 09:26:34 +0545

    Toolbar Stuffs

--- app/src/main/java/viewpager/com/viewpager/MainActivity.java
+++ app/src/main/java/viewpager/com/viewpager/MainActivity.java
@@ -2,15 +2,23 @@ package viewpager.com.viewpager;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeToolbar();
+    }
+
+    private void initializeToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.app_toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
