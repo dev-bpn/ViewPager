cb0fd3ee38bb561c52eecc3886bae4dde106f943
dev-bpn <jazzbpn@gmail.com>
2015-10-18 12:21:32 +0545

    Pass listItem data onSwipe

--- app/src/main/java/viewpager/com/viewpager/fragment/Fragment_A.java
+++ app/src/main/java/viewpager/com/viewpager/fragment/Fragment_A.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import viewpager.com.viewpager.R;
 
@@ -14,7 +16,8 @@ import viewpager.com.viewpager.R;
  */
 public class Fragment_A extends Fragment {
 
-
+    private String[] names = {"name for fragment 1" , "name for fragment 2"};
+    private static ListView listView;
     public Fragment_A() {
     }
 
@@ -26,8 +29,14 @@ public class Fragment_A extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        View layout = inflater.inflate(R.layout.fragment_a, container, false);
+        listView = (ListView) layout.findViewById(R.id.list_view);
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, names));
+        return layout;
     }
 
+     public static Object getItemData(int position){
+         return listView.getItemAtPosition(position);
+    }
 
 }
--- app/src/main/java/viewpager/com/viewpager/fragment/Fragment_B.java
+++ app/src/main/java/viewpager/com/viewpager/fragment/Fragment_B.java
@@ -1,11 +1,13 @@
 package viewpager.com.viewpager.fragment;
 
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import viewpager.com.viewpager.R;
 
@@ -14,7 +16,7 @@ import viewpager.com.viewpager.R;
  */
 public class Fragment_B extends Fragment {
 
-
+    TextView textView;
     public Fragment_B() {
     }
 
@@ -27,8 +29,14 @@ public class Fragment_B extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b, container, false);
+        View layout = inflater.inflate(R.layout.fragment_b, container, false);
+        textView = (TextView) layout.findViewById(R.id.text_view_fragb);
+        return layout;
     }
 
-
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        textView.setText(Fragment_A.getItemData(1).toString());
+    }
 }
