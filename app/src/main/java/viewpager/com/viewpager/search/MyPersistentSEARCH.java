9d8924be341da49e5938448e69c05fb8940db226
dev-bpn <jazzbpn@gmail.com>
2015-11-05 21:28:07 +0545

    search ITEM CLICK

--- app/src/main/java/viewpager/com/viewpager/search/MyPersistentSEARCH.java
+++ app/src/main/java/viewpager/com/viewpager/search/MyPersistentSEARCH.java
@@ -3,12 +3,11 @@ package viewpager.com.viewpager.search;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.quinny898.library.persistentsearch.SearchBox;
 import com.quinny898.library.persistentsearch.SearchResult;
 
+import viewpager.com.viewpager.MainActivity;
 import viewpager.com.viewpager.R;
 
 /**
@@ -18,56 +17,42 @@ public class MyPersistentSEARCH {
 
     public static void openSearch(final SearchBox searchBox  , final Context context , final Toolbar toolbar) {
 
+        String[] names = {"aaa" , "sss" , "ddd" , "aaa" , "sss" , "ddd" , "aaa" , "sss" , "ddd" , "aaa" , "sss" , "ddd"};
         toolbar.setTitle("");
         searchBox.revealFromMenuItem(R.id.action_search, (Activity) context);
         for (int x = 0; x < 10; x++) {
-            String[] names = {"aaa" , "sss" , "ddd" , "aaa" , "sss" , "ddd" , "aaa" , "sss" , "ddd" , "aaa" , "sss" , "ddd"};
-            SearchResult option = new SearchResult(names[x], context.getResources().getDrawable(
-                    R.mipmap.ic_launcher));
+            SearchResult option = new SearchResult(names[x], context.getResources().getDrawable(R.mipmap.ic_launcher));
             searchBox.addSearchable(option);
         }
 
         searchBox.setSearchListener(new SearchBox.SearchListener() {
 
             @Override
-            public void onSearchOpened() {
-                // Use this to tint the screen
-
-            }
+            public void onSearchOpened() {}
 
             @Override
             public void onSearchClosed() {
-                // Use this to un-tint the screen
                 closeSearch(searchBox , context , toolbar);
             }
 
             @Override
-            public void onSearchTermChanged(String term) {
-                // React to the search term changing
-                // Called after it has updated results
-            }
+            public void onSearchTermChanged(String term) { }
 
             @Override
             public void onSearch(String searchTerm) {
-                Toast.makeText(context, searchTerm + " Searched",
-                        Toast.LENGTH_LONG).show();
                 toolbar.setTitle(searchTerm);
-
             }
 
             @Override
             public void onResultClick(SearchResult result) {
-                //React to result being clicked
-                Log.i("GGG" , result.toString());
+                String resString = result.toString();
+
             }
 
             @Override
-            public void onSearchCleared() {
-
-            }
+            public void onSearchCleared() { }
 
         });
-
     }
 
     protected static void closeSearch(SearchBox searchBox , Context context , Toolbar toolbar) {
@@ -75,4 +60,15 @@ public class MyPersistentSEARCH {
         if(searchBox.getSearchText().isEmpty())toolbar.setTitle("");
     }
 
+    private void onSearchItemClick(String result){
+
+        if(result.equals("aaa")){
+            MainActivity.getViewPagerAtCurrentPosition(1);
+        }else if(result.equals("sss")){
+            MainActivity.getViewPagerAtCurrentPosition(2);
+        }else if(result.equals("ddd")){
+            MainActivity.getViewPagerAtCurrentPosition(3);
+        }
+
+    }
 }
